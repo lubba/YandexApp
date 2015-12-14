ab147ee994b396b22364aaf5253696d6d65197fa
Liubov Paina <lubashka.994@mail.ru>
2015-12-14 19:33:53 +0300

    Initial commit

---
+++ yaframe/src/main/java/com/lpaina/yaframe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lpaina.yaframe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
